9687402da8526efd218b7dea00be31cecfcf324b
lavishmanghnani2 <139878421+lavishmanghnani2@users.noreply.github.com>
2024-01-18 18:16:23 +0530

    lat long

--- lib/CharitySkiRace_screen.dart
+++ lib/CharitySkiRace_screen.dart
@@ -3,10 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'controller/charity_race_controller.dart';
-import 'package:whistlerpride/widgets/bottom_navigationBar.dart';
 import 'dart:ui' as ui;
-import 'guidedSkiRideGruops_screen.dart';
+import 'controller/charity_race_controller.dart';
 
 class CharitySkiRaceScreen extends StatefulWidget {
   const CharitySkiRaceScreen({super.key});
@@ -18,6 +16,12 @@ class CharitySkiRaceScreen extends StatefulWidget {
 class _CharitySkiRaceScreenState extends State<CharitySkiRaceScreen> {
 
   final getCharityController = Get.put(GetCharityRaceController());
+  @override
+  void initState() {
+    super.initState();
+    getCharityController.getCharityRace();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -89,7 +93,7 @@ class _CharitySkiRaceScreenState extends State<CharitySkiRaceScreen> {
               const SizedBox(
                 height: 10,
               ),
-               Text(
+              Text(
                 'RACE TO RAISE MONEY FOR CHARITY',
                 style: GoogleFonts.oswald(
                     color: Colors.black,
@@ -99,12 +103,12 @@ class _CharitySkiRaceScreenState extends State<CharitySkiRaceScreen> {
               const SizedBox(
                 height: 10,
               ),
-               Text.rich(
+              Text.rich(
                 TextSpan(
                   children: [
                     TextSpan(
                       text:
-                          'Test your speed and try guessing your time in a friendly race that raises funds for QMUNITY, BC’s Queer, Trans, & Two-Spirit Resource Centre. Show us your sweet skiing or boarding moves as you zoom down the giant dual slalom Race Centre on Blackcomb Mountain.',
+                      'Test your speed and try guessing your time in a friendly race that raises funds for QMUNITY, BC’s Queer, Trans, & Two-Spirit Resource Centre. Show us your sweet skiing or boarding moves as you zoom down the giant dual slalom Race Centre on Blackcomb Mountain.',
                       style:
                       GoogleFonts.robotoSlab(fontWeight: FontWeight.w300, fontSize: 11,height: 1.5,color: Colors.black),
                     ),
@@ -114,66 +118,35 @@ class _CharitySkiRaceScreenState extends State<CharitySkiRaceScreen> {
                         GoogleFonts.robotoSlab(fontSize: 10, color: const Color(0xffE025E7),fontWeight: FontWeight.w600,)),
                   ],
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                ListView.builder(
-                    itemCount: 4,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (index, context) {
-                      return Column(
-                        children: [
-                          Row(
-                            children: [
-                              const CircleAvatar(
-                                minRadius: 5,
-                                maxRadius: 5,
-                                backgroundColor: Color(0xffE025E7),
-                              ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              Text(
-                                'Skiers and snowboarders welcome.',
-                                style: GoogleFonts.robotoSlab(
-                                    color: Colors.black,
-                                    fontSize: 10,
-                                    fontWeight: FontWeight.w400),
-                              ),
-                            ],
-                          ),
-                        ],
-                      );
-                    }),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Prizes will be available for pick-up from the Whistler Pride Welcome Centre at the Aava Hotel.',
-                  style: GoogleFonts.robotoSlab(
-                      color: Colors.black,
-                      fontSize: 11,
-                      fontWeight: FontWeight.w300),
-                ),
-                Container(
-                  padding: const EdgeInsets.all(16.0),
-                  margin: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(11),
-                      boxShadow: const [BoxShadow(blurRadius: 1)]),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // Left side: Image
-                      Container(
-                        margin: const EdgeInsets.only(right: 16.0),
-                        child: Image.asset(
-                          'assets/images/imageis.png',
-                          width: 130.0,
-                          fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ListView.builder(
+                  itemCount: 4,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (index, context) {
+                    return  Column(
+                      children: [
+                        Row(
+                          children: [
+                            const CircleAvatar(
+                              minRadius: 5,
+                              maxRadius: 5,
+                              backgroundColor: Color(0xffE025E7),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Skiers and snowboarders welcome.',
+                              style: GoogleFonts.robotoSlab(
+                                  color: Colors.black,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          ],
                         ),
                         SizedBox(height: 10,)
                       ],
@@ -211,7 +184,7 @@ class _CharitySkiRaceScreenState extends State<CharitySkiRaceScreen> {
                         fit: BoxFit.cover,
                       ),
                     ),
-                     Expanded(
+                    Expanded(
                       child: Text.rich(
                         TextSpan(
                           children: [
@@ -243,8 +216,8 @@ class _CharitySkiRaceScreenState extends State<CharitySkiRaceScreen> {
                           ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
               SizedBox(height: 10,),
@@ -289,41 +262,8 @@ class _CharitySkiRaceScreenState extends State<CharitySkiRaceScreen> {
                                     fontSize: 10,
                                     fontWeight: FontWeight.w300),
                               ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              Expanded(
-                                child: Text(
-                                  'Closest: Ian G. — Guessed Time: 24.82 Actual Time: 24.99',
-                                  style: GoogleFonts.robotoSlab(
-                                      color: Colors.black,
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w300),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      );
-                    }),
-                const SizedBox(height: 10,),
-                Container(
-                  padding: const EdgeInsets.all(16.0),
-                  margin: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(11),
-                      boxShadow: const [BoxShadow(blurRadius: 1)]),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // Left side: Image
-                      Container(
-                        margin: const EdgeInsets.only(right: 16.0),
-                        child: Image.asset(
-                          'assets/images/image.png',
-                          width: 130.0,
-                          fit: BoxFit.cover,
+                            ),
+                          ],
                         ),
                         SizedBox(height: 10,),
                       ],
@@ -410,11 +350,11 @@ class _CharitySkiRaceScreenState extends State<CharitySkiRaceScreen> {
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ) : const Center(child: CircularProgressIndicator(),);
-      }),
+        ),
+      ),
     );
   }
 }
