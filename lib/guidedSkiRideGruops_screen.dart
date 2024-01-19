6e9a764b4c80a8d1bf75624c4bb8c574d501a506
Piyush <deveneoxs@g>
2024-01-19 09:37:33 +0530

    srtrey

--- lib/guidedSkiRideGruops_screen.dart
+++ lib/guidedSkiRideGruops_screen.dart
@@ -5,7 +5,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:whistlerpride/whistlerPrideEats_Screen.dart';
 import 'package:whistlerpride/widgets/bottom_navigationBar.dart';
-
+import 'dart:ui' as ui;
 class GuidedSkiRideGruopsScreen extends StatefulWidget {
   const GuidedSkiRideGruopsScreen({super.key});
 
@@ -27,9 +27,12 @@ class _GuidedSkiRideGruopsScreenState extends State<GuidedSkiRideGruopsScreen> {
             },
             child:  Icon(Icons.arrow_back,color: Colors.black,)),
 
-        title: const Text(
+        title:  Text(
           'Guided Ski & Ride Gruops',
-          style: TextStyle(color: Colors.black, fontSize: 16),
+          style: GoogleFonts.robotoSlab(
+              color: const Color(0xFF000000),
+              fontSize: 16,
+              fontWeight: FontWeight.w400),
         ),
       ),
 
@@ -41,7 +44,44 @@ class _GuidedSkiRideGruopsScreenState extends State<GuidedSkiRideGruopsScreen> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Image.asset('assets/images/imagess.png'),
+          Stack(children: [
+          ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: ColorFiltered(
+            colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(
+                  0.4), // Replace with your desired color and opacity
+              BlendMode.srcATop,
+            ),
+            child: Image.asset(
+              'assets/images/imagess.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+          Container(
+            color: Colors.black, // Adjust opacity as needed
+            child: BackdropFilter(
+              filter:
+              ui.ImageFilter.blur(sigmaX: 1.0, sigmaY: 1.0),
+              child: Container(
+                color: Colors.black.withOpacity(0.3),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 10,
+            left: 10,
+            right: 10,
+            child:  Text(
+              "Guided Ski & Ride gruops",
+              style: GoogleFonts.robotoSlab(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+          ]),
               const SizedBox(
                 height: 10,
               ),
@@ -56,12 +96,13 @@ class _GuidedSkiRideGruopsScreenState extends State<GuidedSkiRideGruopsScreen> {
                 height: 10,
               ),
               Container(
-                padding: const EdgeInsets.all(16.0),
-                margin: const EdgeInsets.all(10),
+
+                padding: const EdgeInsets.all(10.0),
+
                 decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(11),
-                    boxShadow: const [BoxShadow(
+                    boxShadow: const [ BoxShadow(
                       color: Colors.black26,
                       offset: Offset(
                         0.5,
@@ -69,70 +110,67 @@ class _GuidedSkiRideGruopsScreenState extends State<GuidedSkiRideGruopsScreen> {
                       ), //Offset
                       blurRadius: 0.5,
                       spreadRadius: 0.0,
-                    ),]),
+                    ),
+                    ]),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     // Left side: Image
-                    Container(
-                      margin: const EdgeInsets.only(right: 16.0),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
                       child: Image.asset(
                         'assets/images/image.png',
                         width: 130.0,
                         fit: BoxFit.cover,
                       ),
                     ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          // Title
+                    SizedBox(width: 12,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 3,),
+                        // Title
 
-                          Text.rich(
-                            TextSpan(
-                              children: [
-                                TextSpan(
-                                  text:
-                                      'Lift tickets are required for all on-mountain activities and we encourage guests to',
-                                  style: GoogleFonts.robotoSlab(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 10),
-                                ),
-                                TextSpan(
-                                  text: ' Read More',
-                                  style: GoogleFonts.robotoSlab(
-                                      color: Color(0xffE025E7),
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 10),
-                                ),
-                              ],
+                        // Description
+
+                        Text(
+                          'The social core of the week ufffffhigi\nThe social cofsfsffre of the week\nThe social core of the week ufffffhigi',
+                          style: GoogleFonts.roboto(
+                              color: const Color(0xFF000000),
+                              fontSize: 11,
+                              fontWeight: FontWeight.w300),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        // Button
+                        SizedBox(height: 8,),
+                        InkWell(
+                          onTap: () {},
+                          child: Container(
+                            height: 35,
+                            width: 120,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(3),
+                              color: const Color(0xffE025E7),
                             ),
+                            child:  Center(
+                                child: Text(
+                                  'Register',
+                                  style: GoogleFonts.roboto(
+                                      color: const Color(0xFFFFFFFF),
+                                      fontSize: 11,
+                                      fontWeight: FontWeight.w600),
+                                )),
                           ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          // Button
-                          InkWell(
-                            onTap: () {
-                              Get.to(const WhistlerPrideEatsScreen());
-                            },
-                            child: Container(
-                              height: 35,
-                              width: 100,
-                              decoration: BoxDecoration(color:  Color(0xffE025E7),borderRadius: BorderRadius.circular(5)),
-                              child: const Center(
-                                  child: Text(
-                                'Register',
-                                style: TextStyle(
-                                    fontSize: 10, color: Colors.white),
-                              )),
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
+              ),
+              const SizedBox(
+                height: 10,
               ),
                Text(
                 'OUR TICKETS & RENTALS',
@@ -141,89 +179,92 @@ class _GuidedSkiRideGruopsScreenState extends State<GuidedSkiRideGruopsScreen> {
                     fontSize: 18,
                     fontWeight: FontWeight.w600),
               ),
+              const SizedBox(
+                height: 10,
+              ),
               ListView.builder(
                   itemCount: 4,
                   shrinkWrap: true,
                   physics: const NeverScrollableScrollPhysics(),
                   itemBuilder: (index, context) {
-                    return Container(
-                      padding: const EdgeInsets.all(16.0),
-                      margin: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(11),
-                          boxShadow: const [BoxShadow(
-                            color: Colors.black26,
-                            offset: Offset(
-                              0.5,
-                              0.5,
-                            ), //Offset
-                            blurRadius: 0.5,
-                            spreadRadius: 0.0,
-                          ),]),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          // Left side: Image
-                          Container(
-                            margin: const EdgeInsets.only(right: 16.0),
-                            child: Image.asset(
-                              'assets/images/image.png',
-                              width: 130.0,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                // Title
+                    return      Column(
+                      children: [
+                        Container(
 
-                                Text.rich(
-                                  TextSpan(
-                                    children: [
-                                      TextSpan(
-                                        text:
-                                        'Lift tickets are required for all on-mountain activities and we encourage guests to',
-                                        style: GoogleFonts.robotoSlab(
-                                            fontWeight: FontWeight.w600,
-                                            fontSize: 10),
-                                      ),
-                                      TextSpan(
-                                        text: ' Read More',
-                                        style: GoogleFonts.robotoSlab(
-                                            color: Color(0xffE025E7),
-                                            fontWeight: FontWeight.w600,
-                                            fontSize: 10),
-                                      ),
-                                    ],
+                          padding: const EdgeInsets.all(10.0),
+
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(11),
+                              boxShadow: const [ BoxShadow(
+                                color: Colors.black26,
+                                offset: Offset(
+                                  0.5,
+                                  0.5,
+                                ), //Offset
+                                blurRadius: 0.5,
+                                spreadRadius: 0.0,
+                              ),
+                              ]),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // Left side: Image
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.asset(
+                                  'assets/images/image.png',
+                                  width: 130.0,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              SizedBox(width: 12,),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(height: 3,),
+                                  // Title
+
+                                  // Description
+
+                                  Text(
+                                    'The social core of the week ufffffhigi\nThe social cofsfsffre of the week\nThe social core of the week ufffffhigi',
+                                    style: GoogleFonts.roboto(
+                                        color: const Color(0xFF000000),
+                                        fontSize: 11,
+                                        fontWeight: FontWeight.w300),
                                   ),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                // Button
-                                InkWell(
-                                  onTap: () {
-                                    Get.to(const WhistlerPrideEatsScreen());
-                                  },
-                                  child: Container(
-                                    height: 35,
-                                    width: 100,
-                                    decoration: BoxDecoration(color:  Color(0xffE025E7),borderRadius: BorderRadius.circular(5)),
-                                    child: const Center(
-                                        child: Text(
-                                          'Register',
-                                          style: TextStyle(
-                                              fontSize: 10, color: Colors.white),
-                                        )),
+                                  const SizedBox(
+                                    height: 10,
                                   ),
-                                ),
-                              ],
-                            ),
+                                  // Button
+                                  SizedBox(height: 8,),
+                                  InkWell(
+                                    onTap: () {},
+                                    child: Container(
+                                      height: 35,
+                                      width: 120,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(3),
+                                        color: const Color(0xffE025E7),
+                                      ),
+                                      child:  Center(
+                                          child: Text(
+                                            'Register',
+                                            style: GoogleFonts.roboto(
+                                                color: const Color(0xFFFFFFFF),
+                                                fontSize: 11,
+                                                fontWeight: FontWeight.w600),
+                                          )),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
+                        ),
+                        SizedBox(height: 13,),
+                      ],
                     );
                   }),
             ],
